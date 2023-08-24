47fd9810f72bdf9f29386a8351ce5674e0c4705f
Sofía Millán <sofiaisabellamillan16@gmail.com>
2023-08-23 22:53:45 -0500

    Adding navbar and footer components

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <>
+        <footer>
+            <p>Find Us On</p>
+            <div></div>
+            <p>Powered by w3.css</p>
+        </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <>
+    <header>
+      <navbar>
+        <ul className="list">
+          <li><span>img</span>Logo</li>
+          <li><a href="#">Rooms</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <p>Book Now</p>
+      </navbar>
+    </header>
+    </>
+  )
+}
+
+export default Navbar
