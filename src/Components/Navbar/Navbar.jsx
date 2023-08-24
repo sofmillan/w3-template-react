e28504c8ee5d60626dac544c0221ea706bbfb13e
Sofía Millán <sofiaisabellamillan16@gmail.com>
2023-08-24 08:19:31 -0500

    Navbar finished

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,13 +6,13 @@ function Navbar() {
     <>
     <header>
       <navbar>
-        <ul className="list">
-          <li><span>img</span>Logo</li>
-          <li><a href="#">Rooms</a></li>
+        <ul className="navbar-list">
+          <li id="logo"><img src="https://cdn-icons-png.flaticon.com/512/78/78257.png"></img><p>Logo</p></li>
+          <li ><a href="#">Rooms</a></li>
           <li><a href="#">About</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
-        <p>Book Now</p>
+        <button id="book-button">Book Now</button>
       </navbar>
     </header>
     </>
