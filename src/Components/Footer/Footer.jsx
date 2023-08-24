edc66266afe3ae3454af416831490acb680f3ac5
Sofía Millán <sofiaisabellamillan16@gmail.com>
2023-08-24 09:11:21 -0500

    Footer component finished

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,14 @@ function Footer() {
     <>
         <footer>
             <p>Find Us On</p>
-            <div></div>
+            <div className="social-media-logo">
+            <img src="https://assets.stickpng.com/images/60fea6773d624000048712b5.png" />
+            <img src="https://www.edigitalagency.com.au/wp-content/uploads/new-Instagram-logo-white-glyph.png" />
+            <img src="https://www.covetedplaces.com/wp-content/uploads/2015/11/snapchat-logo-white.png"></img>
+            <img src="https://static-00.iconduck.com/assets.00/pinterest-icon-1556x2048-p704qik0.png"></img>
+            <img src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/twitter-icon-18-256.png"></img>
+            <img src="https://www.nobreathfeno.com/wp-content/uploads/2022/01/linkedin.png"></img>
+            </div>
             <p>Powered by w3.css</p>
         </footer>
     </>
