37fc8616082aa8c3f4d3fb8f73aeb828ce0af648
Sofía Millán <sofiaisabellamillan16@gmail.com>
2023-08-25 16:21:10 -0500

    Responsive banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -6,7 +6,11 @@ function Banner() {
   return (
     <>
     <section className="banner">
+    <div className="container">
+    <h1>Hotel Name</h1>
     <SearchForm></SearchForm>
+    </div>
+    
     </section>
     </>
   )
--- src/Components/SearchForm/SearchForm.jsx
+++ src/Components/SearchForm/SearchForm.jsx
@@ -1,5 +1,3 @@
-import './Banner.css'
-
 function SearchForm() {
   return (
     <>
